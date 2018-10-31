d3e9ff1fa597f2af4329cc4395ab12db49698748
Isida Zyun'iti <zyun@moin.jp>
2018-10-31 11:56:54 +0900

    Update prepare-ubuntu-18.04-template.sh

--- prepare-ubuntu-18.04-template.sh
+++ prepare-ubuntu-18.04-template.sh
@@ -82,7 +82,7 @@ sudo sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
 
 # set dhcp to use mac - this is a little bit of a hack but I need this to be placed under the active nic settings
 # also look in /etc/netplan for other config files
-sed -i 's/optional: true/dhcp-identifier: mac/g' /etc/netplan/50-cloud-init.yaml
+sed -i "/^ *dhcp4: true$/i \            dhcp-identifier: mac" /etc/netplan/50-cloud-init.yaml
 
 # cleans out all of the cloud-init cache / logs - this is mainly cleaning out networking info
 sudo cloud-init clean --logs
